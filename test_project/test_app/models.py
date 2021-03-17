4bcc5cb7ac3869d27bdc4e8847be8561a990eab8
Szaszlyk <74797285+Szaszlyk@users.noreply.github.com>
2021-03-17 02:11:07 +0100

    comit

--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -3,5 +3,13 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100,blank=False, null=True)
     created = models.DateTimeField(auto_now_add=True, blank=False, null=True)
+    price = models.IntegerField(default=0)
     def __str__(self):
-        return self.name   
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=True)
+    country = models.CharField(max_length=100, blank=False, null=True)
+    def __str__(self):
+        return self.name
+        
