aff056cf5bd97dbdeab019d74ff9d353dd51b902
Andrzej Padlo <andrzejpadlo@gmail.com>
2021-03-19 18:10:14 +0100

    Slight description enhancements

--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 class Product(models.Model):
-    name = models.CharField(max_length=100,blank=False, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    name = models.CharField(max_length=100,blank=False, null=True,
+    	help_text='Please input product name', 
+    	validators=[MinLengthValidator(4, "Project name should be at least 4 characters, ex. T825")])
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True,
+    	help_text='Please input product price')
     created = models.DateTimeField(auto_now_add=True, blank=False, null=True)
 
     def __str__(self):
